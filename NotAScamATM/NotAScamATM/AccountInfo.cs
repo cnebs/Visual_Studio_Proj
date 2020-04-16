03ff5078ed34174042829c2d49da2ab430cf0b9b
C-Vac <c.vaccarogallo@gmail.com>
2020-04-16 11:10:58 -0400

    aw shit here we go again

---
+++ NotAScamATM/NotAScamATM/AccountInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace NotAScamATM
+{
+	public class AccountInfo
+	{
+		string name;
+		string accountNo;
+		string pinNo;
+		string balance;
+		public string FormattedBalance
+		{
+			get { return string.Format("{0:C}", balance); }
+		}
+
+
+		public AccountInfo(string name, string acc, string pin, string balance)
+		{
+			this.name = name;
+			this.accountNo = acc;
+			this.pinNo = pin;
+			this.balance = balance;
+		}
+
+		public class Accounts
+		{
+			public Accounts(string cardNum, string pin)
+			{
+				CheckCred(cardNum, pin);
+			}
+
+			public bool CheckCred(string cardNum, string pin)
+			{
+
+				for (int i = ClientDB.GetLength(1); i > 0; i--)
+				{
+					if (cardNum == ClientDB[1, i])
+					{
+						if (pin == ClientDB[2, i])
+						{
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+
+			private string[,] ClientDB =
+			{
+				{ "Betty White", "145812", "1488", "14037.59" },
+				{ "Melania Trump", "502314", "1999", "675450112.80"},
+				{ "Davy Jones", "692011", "6969", "3.24"}
+			};
+			
+		}
+	}
+}
---
+++ NotAScamATM/NotAScamATM/SecurityLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NotAScamATM
+{
+    public class SecurityLogin
+    {
+        static void Main()
+        {
+			Console.WriteLine("-----------------------------");
+			Console.WriteLine("| ScamTrust ATM Secure Menu |");
+			Console.WriteLine("|                           |");
+			Console.WriteLine("|                           |");
+			Console.WriteLine("|  Welcome to ScamTrust.    |");
+			Console.WriteLine("|                           |");
+			Console.WriteLine("|  Please insert your card. |");
+			Console.WriteLine("|                           |");
+			Console.WriteLine("-----------------------------");
+			Console.ReadKey(true);
+
+			int attempts = 0;
+			while (attempts < 5) 
+			{
+				Console.Write("Enter your card number: ");
+				string cardNumInput = "_" + Console.ReadLine();
+				Console.Write("\nEnter your four-digit PIN: ");
+				string pinInput = Console.ReadLine();
+
+				AccountInfo.Accounts check = new AccountInfo.Accounts(cardNumInput, pinInput);
+				if (check.CheckCred(cardNumInput, pinInput))
+				{
+					Menu.MainMenu(cardNumInput);
+				}
+				attempts++;
+			}
+		}
+    }
+}
